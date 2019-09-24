a13306fb095770977b0a411c59ac69193da1ac39
Vivek Singh <Vivek.Singh@hanu.com>
2019-09-24 16:35:22 +0530

    Update config.py

--- xbot/config.py
+++ xbot/config.py
@@ -1,6 +1,6 @@
 elb_name = 'TestFinal'
-account_id = 'AKIAVA2LEYH5KHFIV7GZ'
-access_key = 'v5AVnFpADk4TbU0/GKf+Uf2fc+6GOKjU9unWVBzJ'
+account_id = '####################'
+access_key = '####################'
 elb_zones = ['us-east-1a', 'us-east-1b']
 elb_listeners = [[80, 90, 'HTTP']]
 s3_bucket_name = 'kunwarelbbucket'
