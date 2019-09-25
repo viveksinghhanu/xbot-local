2f12e938b3b672ed2b02120eb64f2a8a43faee5a
Kunwar55 <singh.kunwar96@gmail.com>
2019-09-25 17:24:55 +0530

    Update config.py

--- xbot/config.py
+++ xbot/config.py
@@ -1,6 +1,6 @@
 elb_name = 'Demo'
-AWSAccessKey = 'AKIAVA2LEYH5JH7BPNUP'
-AWSSecretKey = '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'
+AWSAccessKey = ''
+AWSSecretKey = ''
 account_id = '123452793804'
 elb_zones = ['us-east-1a', 'us-east-1b']
 elb_listeners = [[80, 90, 'HTTP']]
